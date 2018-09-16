da1be9836590302b4eec2c436c25e859c444c912
Zyracuze <zyracuze@gmail.com>
2018-09-16 14:51:44 +0700

    [CHANGED] Rename the constand from VATRATE to VAT_RATE in function calculateVATE

--- calculateVAT/calculateVAT.py
+++ calculateVAT/calculateVAT.py
@@ -2,7 +2,7 @@ VAT_RATE = 7.0
 
 def calculateVAT(productPrice):
     if productPrice >= 0:
-        vatValue = (productPrice * VATRATE)/100
+        vatValue = (productPrice * VAT_RATE)/100
         return vatValue
     else: return 'Can not calculate VAT because the product price is minus.'
      
